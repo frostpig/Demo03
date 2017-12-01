7ba2595b394cde64143390319a3fb3ed2bd2fff2
frostpig <1397233369@qq.com>
2017-12-01 17:17:20 +0800

    20171201

--- src/main/java/com/lss/controller/UserController.java
+++ src/main/java/com/lss/controller/UserController.java
@@ -45,13 +45,15 @@ public class UserController {
     @RequestMapping(value = "/check1", method = RequestMethod.GET)
     public Object check1() {
         String data = new String("{\"total\":7,\"rows\":[\n" +
-                "\t{\"id\":1,\"name\":\"All Tasks\",\"begin\":\"3/4/2010\",\"end\":\"3/20/2010\",\"progress\":60,\"iconCls\":\"icon-ok\"},\n" +
-                "\t{\"id\":2,\"name\":\"Designing\",\"begin\":\"3/4/2010\",\"end\":\"3/10/2010\",\"progress\":100,\"_parentId\":1,\"state\":\"closed\"},\n" +
-                "\t{\"id\":21,\"name\":\"Database\",\"persons\":2,\"begin\":\"3/4/2010\",\"end\":\"3/6/2010\",\"progress\":100,\"_parentId\":2},\n" +
-                "\t{\"id\":22,\"name\":\"UML\",\"persons\":1,\"begin\":\"3/7/2010\",\"end\":\"3/8/2010\",\"progress\":100,\"_parentId\":2},\n" +
-                "\t{\"id\":23,\"name\":\"Export Document\",\"persons\":1,\"begin\":\"3/9/2010\",\"end\":\"3/10/2010\",\"progress\":100,\"_parentId\":2},\n" +
-                "\t{\"id\":3,\"name\":\"Coding\",\"persons\":2,\"begin\":\"3/11/2010\",\"end\":\"3/18/2010\",\"progress\":80},\n" +
-                "\t{\"id\":4,\"name\":\"Testing\",\"persons\":1,\"begin\":\"3/19/2010\",\"end\":\"3/20/2010\",\"progress\":20}\n" +
+                "\t{\"id\":2,\"name\":\"All Tasks\",\"begin\":\"3/4/2010\",\"end\":\"3/20/2010\",\"progress\":60,\"iconCls\":\"icon-ok\"},\n" +
+                "\t{\"id\":3,\"name\":\"Designing\",\"begin\":\"3/4/2010\",\"end\":\"3/10/2010\",\"progress\":100,\"_parentId\":2,\"state\":\"closed\"},\n" +
+                "\t{\"id\":21,\"name\":\"Database\",\"persons\":2,\"begin\":\"3/4/2010\",\"end\":\"3/6/2010\",\"progress\":100,\"_parentId\":3},\n" +
+                "\t{\"id\":22,\"name\":\"UML\",\"persons\":1,\"begin\":\"3/7/2010\",\"end\":\"3/8/2010\",\"progress\":100,\"_parentId\":3},\n" +
+                "\t{\"id\":23,\"name\":\"Export Document\",\"persons\":1,\"begin\":\"3/9/2010\",\"end\":\"3/10/2010\",\"progress\":100,\"_parentId\":3},\n" +
+                "\t{\"id\":4,\"name\":\"Coding\",\"persons\":2,\"begin\":\"3/11/2010\",\"end\":\"3/18/2010\",\"progress\":80,\"_parentId\":2,\"state\":\"closed\"},\n" +
+                "\t{\"id\":31,\"name\":\"子节点1\",\"persons\":2,\"begin\":\"3/11/2010\",\"end\":\"3/18/2010\",\"spend\":\"30ms\",\"progress\":80,\"_parentId\":4},\n" +
+                "\t{\"id\":32,\"name\":\"子节点2\",\"persons\":2,\"begin\":\"3/11/2010\",\"end\":\"3/18/2010\",\"spend\":\"30ms\",\"progress\":80,\"_parentId\":4},\n" +
+                "\t{\"id\":5,\"name\":\"Testing\",\"persons\":1,\"begin\":\"3/19/2010\",\"end\":\"3/20/2010\",\"progress\":20}\n" +
                 "],\"footer\":[\n" +
                 "\t{\"name\":\"Total Persons:\",\"persons\":7,\"iconCls\":\"icon-sum\"}\n" +
                 "]}\n");
