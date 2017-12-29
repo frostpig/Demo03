0285166bbd261b939222eefa74d62eff515f1d31
frostpig <1397233369@qq.com>
2017-12-29 18:27:20 +0800

    20171229

--- src/main/java/com/lss/controller/UserController.java
+++ src/main/java/com/lss/controller/UserController.java
@@ -2,14 +2,10 @@ package com.lss.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.lss.service.UserService;
 import org.apache.commons.collections.map.HashedMap;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
